167ad06fe0162252799753abc1fed91941697c24
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2021-11-27 11:49:48 +0700

    update

---
+++ src/04_module_exports/greet.js
@@ -0,0 +1,12 @@
+const greet = () => {
+  console.log('hello world')
+}
+
+const greet1 = () => {
+  console.log('greet 1')
+}
+
+module.exports = {
+  greet,
+  greet1,
+}
---
+++ src/04_module_exports/index.js
@@ -0,0 +1,4 @@
+const { greet, greet1 } = require('./greet')
+
+greet()
+greet1()
