982e60e27fa76dab175a1a99cec17ceffc70fbb1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2021-11-27 13:12:45 +0700

    update

--- src/04_module_exports/index.js
+++ src/04_module_exports/index.js
@@ -1,8 +1,17 @@
 const { greet, greet1 } = require('./greet') // module.exports
 const { order, payment } = require('./product')
+const persons = require('./data.json')
+const config = require('./config')
 
 greet()
 greet1()
 
 order()
 payment()
+
+console.log(persons.map(x => ({ ...x, name: `mapped ${x.name}` })))
+
+console.log(config)
+config.baseUrl = 'updated baseUrl'
+
+console.log(require('./config'))
